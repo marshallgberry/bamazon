76646406283759a6a2a8341760e8653e7563ecaa
Marshall Berry <marshallgberry@gmail.com>
2018-07-27 23:20:54 -0400

    .js/mysql updated, .gitignore created

---
+++ bamazonCustomer.js
@@ -0,0 +1,84 @@
+
+//required packages
+const mysql = require("mysql");
+const inquirer = require('inquirer');
+const Table = require('easy-table');
+
+//connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "10425ber",
+  database: "bamazon_Db"
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  // console.log("connected as id " + connection.threadId);
+  query();
+});
+
+function query() {
+  var query = connection.query("SELECT * FROM products", function (err, res) {
+    if (err) {
+      throw err;
+    } else {
+
+      var response = res;
+      console.log('');
+      console.log('$$$$$$$$$$$$$$$$$$$$ WELCOME TO BAMAZON $$$$$$$$$$$$$$$$$$$$');
+      // npm package to log database into table in console
+      var t = new Table
+      response.forEach(function (product) {
+        t.cell('Product ID', product.item_id)
+        t.cell('Product Name', product.product_name)
+        t.cell('Department', product.department_name)
+        t.cell('Price', product.product_price, Table.number(2))
+        t.cell('Quantity', product.stock_qty)
+        t.newRow()
+      })
+      console.log(t.toString());
+      console.log('$$$$$$$$$$$$$$$$$$$$ WELCOME TO BAMAZON $$$$$$$$$$$$$$$$$$$$');
+    }
+    console.log('');
+    prompt();
+  })
+};
+
+function prompt() {
+  inquirer.prompt([{
+      type: "input",
+      message: "Please type ID of the product to buy: ",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "Please type quantity of the product to buy: ",
+      name: "quantity"
+    }
+  ]).then(function (input) {
+    var buyerID = input.id;
+    var buyerQty = input.quantity;
+
+    var query = connection.query(`SELECT * FROM products WHERE item_ID = ${buyerID}`, function (err, res) {
+      if (err) {
+        throw err;
+      } else if (buyerQty <= res[0].stock_qty) {
+        var totalCost = buyerQty * res[0].product_price;
+        // console.log(res) // TESTING
+        var query1 = connection.query(`UPDATE products SET stock_qty = stock_qty - ${buyerQty} WHERE item_ID = ${buyerID}`,
+          function (err, res) {
+            console.log(`Order completed, your total cost is $${totalCost}.00`)
+            console.log('')
+            prompt();
+          }
+        )
+      } else if (buyerQty > res[0].stock_qty) {
+        console.log('Order incomplete, insufficient quanitity')
+        console.log('')
+        prompt();
+      }
+    })
+  })
+}
---
+++ bamazon_Db.sql
@@ -0,0 +1,24 @@
+ DROP DATABASE IF EXISTS bamazon_Db;
+ CREATE database bamazon_Db;
+
+ USE bamazon_Db;
+
+ CREATE TABLE products (
+   item_id INT AUTO_INCREMENT NOT NULL,
+   product_name VARCHAR(100) NULL,
+   department_name VARCHAR(100) NULL,
+   product_price DECIMAL(10,2) NULL,
+   stock_qty INTEGER (11) NULL,
+   PRIMARY KEY (item_ID)
+ );
+
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, product_price, stock_qty)
+	VALUES
+    ('Fender American Jazzmaster', 'musical instruments', 1500, 3),
+    ('Fender 68 Custom Twin Reverb Amp' , 'musical instruments', 1200, 7),
+	('Nikon D7200 DSLR Camera', 'electronics', 1000, 13),
+    ('Nikon AF-S FX NIKKOR 50mm f/1.8G Lens ', 'electronics', 250, 9),
+    ('KitchenAid Professional Stand Mixer' , 'home and kitchen', 300, 51),
+    ('iRobot Roomba 671 Robot Vacuum', 'home and kitchen', 400, 37);
